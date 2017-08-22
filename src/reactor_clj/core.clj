1187810f0ea499edc317eeadd895bc115582fc1c
Kevin Baldor <kbaldor@Kevins-MacBook-Pro.local>
2017-08-22 08:38:25 -0500

    Now with code for extracting subexpressions.

--- src/reactor_clj/core.clj
+++ src/reactor_clj/core.clj
@@ -1,9 +1,10 @@
 (ns reactor-clj.core
-  (:use [clojure.set]
+  (:use [clojure.set :refer [intersection]]
         [clojure.walk]
         [reactor-clj.type-check :refer :all]
         [clojure.core.match :refer [match]]
-        [clojure.pprint :refer [pprint]]))
+        [clojure.pprint :refer [pprint]]
+        [clojure.string :refer [join]]))
 
 (def type-decls 
   (let [math-types       '(compound ((int int) int)
@@ -58,12 +59,25 @@
   (match reactor-type
          'int 'Int))
 
+(defn SMTLIB-var-type [var-reactor-type]
+  (let [[var reactor-type] var-reactor-type]
+    (list var (SMTLIB-type reactor-type))))
+
+(defn format-assertion [assertion]
+  (match (into []  assertion)
+         ['forall vars statement]
+         (list 'forall (map SMTLIB-var-type (partition 2 vars)) 
+               (format-assertion statement))
+         :else assertion))
+
 (defn update-SMTLIB-commands [commands statement]
   (match (into [] statement)
          ['fn name parameter-types return-type]
          (conj commands (list 'declare-fun name
                               (map SMTLIB-type parameter-types)
                               (SMTLIB-type return-type)))
+         ['forall & rest] 
+         (conj commands (list 'assert (format-assertion statement)))
          :else commands))
 
 (defn update-type-decls [type-decls statement]
@@ -71,9 +85,6 @@
          ['fn name parameter-types return-type]
          (assoc type-decls name (list 'simple parameter-types return-type))
          :else type-decls))
-
-(update-SMTLIB-commands [] '(fn max (int int) int))
-(update-type-decls {} '(fn max (int int) int))
 
 (defn process-function-property [[commands type-decls] statement]
   [(update-SMTLIB-commands commands statement)
@@ -90,14 +101,43 @@
           [commands type-decls] 
           (rest function-properties-block)))
 
+(defn sub-divisible? [statement]
+  (if (sequential? statement) 
+    (match (into [] statement)
+           ['fn* & rest] false
+           ['fn  & rest] false
+           :else true)
+    false))
+
+(defn get-subexpressions [name expression]
+  (let [[expression index children]
+        (reduce (fn [[expression index children] child]
+                  (if (sub-divisible? child)
+                    (let [sub-name (symbol (str name "-" index))] 
+                      [(conj expression sub-name)
+                       (inc index)
+                       (conj children [sub-name child])])
+                    [(conj expression child)
+                     index
+                     children])) [[(first expression)] 1 []] (rest expression))]
+    (reduce into [[name (apply list expression)]] 
+            (map (partial apply get-subexpressions) children))))
+
+;(get-subexpressions 'X '(filter (fn [x] (> x 5)) (+ 1 (- 2 3) 4 (* 5 (/ 6 7)))))
+
 (defn process-reactor [[commands type-decls] form]
-  (let [name             (second form)
-        decls            (rest (rest form))
-        [input decls]    (get-section :input   decls)
-        [output private] (get-section :output  decls)
-        decls            (extract-decls input output private)]
-    (println "decls:" decls)
-    (println "meta decls:" (meta (first decls)))
+  (let [name                  (second form)
+        decls                 (rest (rest form))
+        [input         decls] (get-section :input   decls)
+        [output        decls] (get-section :output  decls)
+        [assumes       decls] (get-section :assumes  decls)
+        [gurarantees private] (get-section :guarantees  decls)
+        decls                 (extract-decls input output private)]
+    (println private)
+    (doseq [[name expr] private]
+      (println name)
+      (doseq [[name expr] (get-subexpressions name expr)]
+        (println name expr)))
     (println "found inputs:")
     (doseq [e input]
       (println "  " e))
@@ -108,6 +148,8 @@
     (doseq [e private]
       (println "  " e (extract-dependencies decls e))))
   [commands type-decls])
+
+(verify-reactor-file "test/volume-1.rct" {})
 
 (defn process-form [[commands type-decls] form]
   (println "working with form")
@@ -128,7 +170,8 @@
                                             (#(str "[" %1 "]"))
                                             (read-string)))]
       (pprint commands)
-      (pprint type-decls))
+      (pprint type-decls)
+      (spit (join [filename ".smt"]) (join "\n" (map str commands))))
     (in-ns old-ns-name)))
 
 (verify-reactor-file "test/volume-1.rct" {})
